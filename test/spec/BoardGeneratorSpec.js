bf204de7ac0a05ccb45f9eb672f3a06736c725ea
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-05-21 18:19:08 +0200

    Typo

--- test/spec/BoardGeneratorSpec.js
+++ test/spec/BoardGeneratorSpec.js
@@ -1,6 +1,6 @@
 describe("BoardGenerator", function() {
   describe("generateSquare", function() {
-    it("generates sqaure", function() {
+    it("generates square", function() {
       var s = BoardGenerator.generateSquare(4, 5)
       expect(+s.getAttribute("data-x")).toEqual(4);
       expect(+s.getAttribute("data-y")).toEqual(5);
